0b92ac73600b5a5417720e578460bc8a9fa1571f
HelloMyVan <fsl971976437@outlook.com>
2017-04-11 22:52:46 +0800

    Java update.

---
+++ JavaLearnApp_1/src/javalearnapp_1/JavaLearnApp_1.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javalearnapp_1;
+import java.io.*;
+import java.util.*;
+
+/**
+ *
+ * @author fsl97
+ */
+public class JavaLearnApp_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is JavaLearnAppp_1!");
+        Scanner NumReader=new Scanner(System.in);
+        int a,b;
+
+            a=NumReader.nextInt();
+            b=NumReader.nextInt();
+
+
+            System.out.println("a="+a);
+            System.out.println("b="+b);
+            System.out.println("a+b="+(a+b));
+
+        // TODO code application logic here
+    }
+    
+}
